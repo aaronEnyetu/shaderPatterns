e5f27b6dbf797452ec64ab435a9e7e54700f91b0
Aaron Enyetu <aaronenyetu@gmail.com>
2023-04-26 16:13:29 -0700

    Pattern 3 complete

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -3,5 +3,6 @@ varying vec2 vUv;
 void main()
 {
     // gl_FragColor = vec4(vUv, 1.0, 1.0); //Pattern 1
-    gl_FragColor = vec4(vUv, 0.5, 1.0);
+    // gl_FragColor = vec4(vUv, 0.5, 1.0); //Pattern 2
+        gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0); //Pattern 3
 }
