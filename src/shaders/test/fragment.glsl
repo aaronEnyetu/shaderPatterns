60d9d88a3b2e2ec58d6b9fb71a4e10cd83edc197
Aaron Enyetu <aaronenyetu@gmail.com>
2023-04-26 16:45:05 -0700

    Pattern 10 complete

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -26,7 +26,12 @@ void main()
 // strength = step(0.5, strength); //you can use if/else statement but this is better for optimization
 
 //Pattern 9
-float strength = mod(vUv.y * 10.0, 1.0);
+// float strength = mod(vUv.y * 10.0, 1.0);
+// strength = step(0.8, strength);
+
+
+//Pattern 10
+float strength = mod(vUv.x * 10.0, 1.0);
 strength = step(0.8, strength);
 
  gl_FragColor = vec4(strength, strength, strength, 1.0);
