8c37e38ab3dc5625d98d006aca201e3a26ceb176
Aaron Enyetu <aaronenyetu@gmail.com>
2023-04-26 16:18:21 -0700

    Pattern 4 complete

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -4,5 +4,13 @@ void main()
 {
     // gl_FragColor = vec4(vUv, 1.0, 1.0); //Pattern 1
     // gl_FragColor = vec4(vUv, 0.5, 1.0); //Pattern 2
-        gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0); //Pattern 3
+   // gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0); //Pattern 3
+
+//Pattern 3
+// float strength = vUv.x;
+
+//Pattern 4
+float strength = vUv.y;
+
+ gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
