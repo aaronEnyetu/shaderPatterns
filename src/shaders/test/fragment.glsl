9e9d64759506a04e22a37d045b72533dd1ec321e
Aaron Enyetu <aaronenyetu@gmail.com>
2023-04-26 18:07:29 -0700

    Pattern 23 complete

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,5 +1,11 @@
 varying vec2 vUv;
 
+
+float random(vec2 st) 
+{
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
 void main()
 {
     // gl_FragColor = vec4(vUv, 1.0, 1.0); //Pattern 1
@@ -92,8 +98,11 @@ void main()
 
 //Pattern 22
 
-float strength = floor(vUv.x * 10.0) / 10.0;
-strength *= floor(vUv.y * 10.0) / 10.0;
+// float strength = floor(vUv.x * 10.0) / 10.0;
+// strength *= floor(vUv.y * 10.0) / 10.0;
+
+//Pattern 23
+float strength = random(vUv);
 
  gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
