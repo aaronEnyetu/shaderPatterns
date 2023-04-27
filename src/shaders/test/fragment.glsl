fd8ea743f0d4f382620d964da9f8f0992eca35ed
Aaron Enyetu <aaronenyetu@gmail.com>
2023-04-26 17:37:20 -0700

    Pattern 18 complete

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -70,7 +70,11 @@ void main()
 
 //Pattern 17
 
-float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5)) ;
+// float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5)) ;
+
+//Pattern 18
+
+float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)) ;
 
 
  gl_FragColor = vec4(strength, strength, strength, 1.0);
