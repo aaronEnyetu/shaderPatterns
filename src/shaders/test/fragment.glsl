b9b3da1a76f14c8624142e3ea089b6581c428916
Aaron Enyetu <aaronenyetu@gmail.com>
2023-04-27 22:43:40 -0700

    Pattern 43 complete

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -236,9 +236,18 @@ void main()
 // float strength = angle;
 
 //Pattern 42
+// float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+// angle /= PI * 2.0;
+// angle+= 0.5;
+// float strength = angle;
+
+
+//Pattern 43
 float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
 angle /= PI * 2.0;
 angle+= 0.5;
+angle *= 20.0;
+angle = mod(angle, 1.0);
 float strength = angle;
 
 
