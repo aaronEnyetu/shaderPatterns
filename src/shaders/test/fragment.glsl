5fcba2127e023b19164b3de71ae100d6519a8ae9
Aaron Enyetu <aaronenyetu@gmail.com>
2023-04-27 22:36:50 -0700

    Pattern 42 complete

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -231,9 +231,16 @@ void main()
 // float angle = atan(vUv.x, vUv.y);
 // float strength = angle;
 
-//Pattern 40
+//Pattern 41
+// float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+// float strength = angle;
+
+//Pattern 42
 float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+angle /= PI * 2.0;
+angle+= 0.5;
 float strength = angle;
+
 
  gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
